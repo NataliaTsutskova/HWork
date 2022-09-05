efdd8a45b0b073a03a7d87038b96b06629129d47
Natalia Tsutskova <purple_sky@list.ru>
2022-09-05 17:43:39 +0300

    add new homework

---
+++ # 2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача № 1
+
+int n = new Random().Next(100, 1000);
+System.Console.WriteLine(n);
+
+int sd = n / 10 % 10;
+System.Console.WriteLine($"Вторая цифра числа {n} равна {sd}");
+
+// Задача № 2
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int td = num / 100 % 10;
+
+if (td > 0)
+{
+    System.Console.WriteLine($"Третья цифра числа {num} равна {td}");
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+
+// Задача № 3
+
+int d = new Random().Next(1, 8);
+System.Console.WriteLine(d);
+
+if (d <= 5)
+{ 
+    System.Console.WriteLine("Данный день недели не является выходным");
+}
+else
+{
+    System.Console.WriteLine("Данный день недели является выходным");
+}
