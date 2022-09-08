c56a8f5e4ed9d15a40ce097475607ec3840b38af
Natalia Tsutskova <purple_sky@list.ru>
2022-09-08 21:25:29 +0300

    Homework № 3

---
+++ # 3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача № 1
+
+System.Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n / 10000 == n % 10 && n / 1000 % 10 == n / 10 % 10)
+{
+    System.Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    System.Console.WriteLine("Число не является палиндромом");
+}
+
+// Задача 2
+
+System.Console.WriteLine("Введите координаты первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите координаты второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+System.Console.WriteLine(result);
+
+// Задача 3
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}
