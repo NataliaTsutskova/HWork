1e302dc0fc8a88c254c998f160545001a660db97
Natalia Tsutskova <purple_sky@list.ru>
2022-09-15 22:43:46 +0300

    5

--- # 5/Program.cs
+++ # 5/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 1
+
+        int[] GetArray() {
+            int[] result = new int[10];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = new Random().Next(100, 1000);
+            }
+            return result;
+        }
+        void PrintArray(int[] arr) {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.WriteLine();
+        }
+        int CountEvenNumbers(int[] array) {
+            int count = 0;
+
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] % 2 == 0)
+                    count += 1;
+            }
+            return count;
+        }
+        var array = GetArray();
+        PrintArray(array);
+        var EvenCount = CountEvenNumbers(array);
+        Console.WriteLine(EvenCount);
+    
+// Задача № 2
+
+
+
+
+
